42eacfba249b0f41c721f85eddc9761b919bd528
Joël Lamotte <joel@jellynote.com>
2021-09-09 13:27:13 +0200

    Fixed configs

--- init-configs.sh
+++ init-configs.sh
@@ -1,6 +1,6 @@
 rm -rf ../build-* .bdep/
 
-bdep init -C ../build-clang @gcc cc --options-file config-gcc.options
+bdep init -C ../build-gcc @gcc cc --options-file config-gcc.options
 bdep init -C ../build-clang @clang cc --options-file config-clang.options
 bdep init -C ../build-wasm @wasm cc --options-file config-wasm.options
 
