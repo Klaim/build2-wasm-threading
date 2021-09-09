9dfe0325b60590e47ccd28313335d72a0e94dc39
Joël Lamotte <joel@jellynote.com>
2021-09-09 16:05:43 +0200

    Attempt to spawn a lot of threads (works with Emscripten's node)

--- threading/threading.cxx
+++ threading/threading.cxx
@@ -2,6 +2,7 @@
 #include <atomic>
 #include <thread>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -36,13 +37,17 @@ main (int argc, char* argv[])
   if(argc == 2 && std::string(argv[1]) == "-v")
     verbose = true;
 
+  vector<thread> threads;
 
-  thread t1 (f, 4000), t2 (f, 3000), t3 (f, 2000), t4 (f, 1000);
+  for(int tidx = 0; tidx < 57; ++tidx)
+  {
+    threads.push_back(thread(f, 4000));
+    threads.push_back(thread(f, 3000));
+    threads.push_back(thread(f, 2000));
+    threads.push_back(thread(f, 1000));
+  }
 
-  t1.join ();
-  t2.join ();
-  t3.join ();
-  t4.join ();
+  for(auto& t : threads) t.join();
 
   if(verbose) cout << '\n';
   cout << n << endl;
